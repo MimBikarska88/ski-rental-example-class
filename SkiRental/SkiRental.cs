843ddce4f5199b31d2d2b4608f6df23516b26712
Maria Bikarska <92679864+MimBikarska88@users.noreply.github.com>
2021-10-18 09:45:12 +0300

    Add files via upload

---
+++ SkiRental/SkiRental.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+
+    public class SkiRental 
+    {
+        public List<Ski> data;
+        public int Capacity { get; set; }
+        public string Name { get; set; }
+
+        public SkiRental(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            data = new List<Ski>();
+        }
+        public void Add(Ski ski)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(ski);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            var findSki = data.FindIndex(ski => ski.Manufacturer.Equals(manufacturer) && ski.Model.Equals(model));
+            if (findSki > -1)
+            {
+                data.RemoveAt(findSki);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Ski GetNewestSki() =>  data.OrderByDescending(ski => ski.Year).FirstOrDefault();
+
+        public Ski GetSki(string manufacturer, string model) =>
+            data.FirstOrDefault(ski => ski.Manufacturer == manufacturer && ski.Model == model);
+
+        public int Count
+        {
+            get => data.Count;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"The skis stored in {Name}:");
+            foreach (var ski in data)
+            {
+                stringBuilder.AppendLine(ski.ToString());
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ SkiRental/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SkiRental
+{
+    public class StartUp
+    {
+        /*
+ * Summer has come!
+ * Your friend who owns a Ski Rental business asked you to write a program to
+ * help him organize his ski storage in order to be prepared for the coming winter season.
+ *
+ * 
+ */
+        static void Main(string[] args)
+        {
+            // Initialize the repository
+            SkiRental skiRental = new SkiRental("New Alpine ski rental", 5);
+
+                // Initialize entity
+            Ski firstSkiSet = new Ski("Rossignol", "XC70", 2017);
+
+                // Print Ski
+            Console.WriteLine(firstSkiSet); // Rossignol - XC70 - 2017
+
+                // Add Ski
+            skiRental.Add(firstSkiSet);
+
+                // Remove Ski
+            Console.WriteLine(skiRental.Remove("Rossignol", "XC90")); // False
+            Console.WriteLine(skiRental.Remove("Rossignol", "XC70")); // True
+
+            Ski secondSkiSet = new Ski("Fischer", "SpeedMax", 2018);
+            Ski thirdSkiSet = new Ski("Salomon", "TopLine", 2020);
+
+            skiRental.Add(secondSkiSet);
+            skiRental.Add(thirdSkiSet);
+
+                // Get Newest Ski
+            Ski newestSki = skiRental.GetNewestSki();
+            Console.WriteLine(newestSki); // Salomon - TopLine - 2020
+
+                // Get Ski
+            Ski salomonTopLine = skiRental.GetSki("Salomon", "TopLine");
+            Console.WriteLine(salomonTopLine); // Salomon - TopLine - 2020
+
+                // Count
+            Console.WriteLine(skiRental.Count); // 2
+
+            // Get Statistics
+            Console.WriteLine(skiRental.GetStatistics());
+            // The skis stored in New Alpine ski rental:
+            // Fischer - SpeedMax – 2018
+            // Salomon - TopLine - 2020
+
+
+        }
+    }
+}
